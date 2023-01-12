13b939c50595e937ba5bc67f3ab11ba01b8e1ff7
Timofey Kushnirenko <102825115+Timo4ka1@users.noreply.github.com>
2023-01-12 21:13:21 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+"use strict"
+
+console.log('=====Домашка 1=====');
+console.log('Hello world');
+console.log('Я yчу JavaScript');
+console.log('==========');
+
+//
+
+console.log('=====Домашка 2=====');
+console.log('Учим JS');
+
+console.log('JS')
+console.log('Учим')
+
+console.log('Учим');
+console.log('JS');
+console.log('==========');
+
+//
+
+console.log('=====Домашка 3=====');
+let eyeColor = 'green';
+console.log(eyeColor);
+
+let username, user;
+username = "Вася";
+user = username;
+console.log(user);
+
+console.log('==========');
+
+//
+
+console.log('=====Домашка 4=====');
+let userAge = 36;
+let userInfo = `Фрилансер ${userAge}`;
+console.log(userInfo)
+
+let userHeight = 145 / 0;
+console.log(userHeight);
+
+let userName;
+console.log(typeof userName);
+
+
+let userSize = "45" / "8";
+console.log(typeof userSize);
+
+console.log('==========');
